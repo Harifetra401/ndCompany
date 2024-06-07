895694071a0df8ac5e4962e6eb01d0ff9748c019
Harifetra401 <fitahiantsoaharifetraars@gmail.com>
2024-06-07 11:30:49 +0200

    sac carton

--- html/index.php
+++ html/index.php
@@ -201,5 +201,3 @@ require('date.php');
   <script src="../assets/vendor/libs/popper/popper.js"></script>
   <script src="../assets/vendor/js/bootstrap.js"></script>
   <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-
-  <
